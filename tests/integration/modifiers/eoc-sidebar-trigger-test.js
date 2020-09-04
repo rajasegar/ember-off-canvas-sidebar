6d7aab79661332a10db4bd823f43f19faa178ad6
Rajasegar <rajasegar.c@gmail.com>
2020-09-04 11:50:36 +0530

    tests: add tests to improve coverage

--- tests/integration/modifiers/eoc-sidebar-trigger-test.js
+++ tests/integration/modifiers/eoc-sidebar-trigger-test.js
@@ -1,15 +1,64 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, find, waitUntil } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Modifier | eoc-sidebar-trigger', function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
+  const markup = hbs`
+<div id="st-container" class="st-container">
+  <EocSidebar @effect="st-effect-slide-in-on-top">
+    <h2 class="icon icon-lab">Sidebar</h2>
+    <ul>
+      <li><a class="icon icon-data" href="#">Data Management</a></li>
+      <li><a class="icon icon-location" href="#">Location</a></li>
+      <li><a class="icon icon-study" href="#">Study</a></li>
+      <li><a class="icon icon-photo" href="#">Collections</a></li>
+      <li><a class="icon icon-wallet" href="#">Credits</a></li>
+    </ul>
+  </EocSidebar>
+    <div class="st-content">
+      <div class="st-content-inner">
+
+            <button type="button"  {{eoc-sidebar-trigger "st-effect-slide-in-on-top"}}>Slide in on top</button>
+            </div>
+            </div>
+            </div>
+
+  `;
+
   test('it renders', async function(assert) {
-    await render(hbs`<div {{eoc-sidebar-trigger}}></div>`);
+    await render(markup);
 
     assert.ok(true);
+  });
+
+  test('it shows menu', async function(assert) {
+
+    await render(markup);
+
+    await click('button');
+    await waitUntil(function() {
+      return find('.st-container').classList.contains('st-menu-open');
+    }, { timeout: 25 })
+
+    assert.ok(find('.st-container').classList.contains('st-menu-open'));
+    assert.ok(find('.st-container').classList.contains('st-effect-slide-in-on-top'));
+
+  });
+
+  test('it closes menu', async function(assert) {
+
+    await render(markup);
+
+    await click('button');
+    await waitUntil(function() {
+      return find('.st-container').classList.contains('st-menu-open');
+    }, { timeout: 25 })
+
+    await click('#st-container');
+    assert.notOk(find('.st-container').classList.contains('st-menu-open'));
+
   });
 });
