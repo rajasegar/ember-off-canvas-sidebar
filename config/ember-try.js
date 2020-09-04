c326d7b48ca46bc1b1033dbd22fbb8ac9528a45a
Rajasegar <rajasegar.c@gmail.com>
2020-09-04 12:10:10 +0530

    chore(ci): removing unnecessary ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,14 +6,6 @@ module.exports = async function() {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
@@ -37,14 +29,6 @@ module.exports = async function() {
           }
         }
       },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
       // The default `.travis.yml` runs this scenario via `npm test`,
       // not via `ember try`. It's still included here so that running
       // `ember try:each` manually or from a customized CI config will run it
@@ -53,19 +37,6 @@ module.exports = async function() {
         name: 'ember-default',
         npm: {
           devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
-          }
         }
       },
       {
